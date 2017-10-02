9ef58c287b6927a60e1d0985562ef0ec481ba3f4
Ryan Schwers <schwers.r@gmail.com>
2017-10-01 18:50:30 -0700

    Use insertRule  in browser

--- src/models/BrowserStyleSheet.js
+++ src/models/BrowserStyleSheet.js
@@ -23,14 +23,22 @@ import StyleSheet, { SC_ATTR, LOCAL_ATTR } from './StyleSheet'
 
 export const COMPONENTS_PER_TAG = 40
 
+const IS_BROWSER = typeof window !== 'undefined'
+const IS_DEV = (process.env.NODE_ENV === 'development') || (!process.env.NODE_ENV)
+const USE_SPEEDY = IS_BROWSER && !IS_DEV
+
+// `insertRule` only accepts one css rule per call.
+// A rule can either be one selector, one @import, or one @media (with nested selectors)
+const CSS_RULE_SPLIT_RE = /((@import\.*?;)|(@media\(.*?\)\{\..+?\{.*?\}\})|(\..+?\{.*?\}))/g
+
 class BrowserTag implements Tag {
   isLocal: boolean
   components: { [string]: Object }
   size: number
-  el: HTMLElement
+  el: HTMLStyleElement
   ready: boolean
 
-  constructor(el: HTMLElement, isLocal: boolean, existingSource: string = '') {
+  constructor(el: HTMLStyleElement, isLocal: boolean, existingSource: string = '') {
     this.el = el
     this.isLocal = isLocal
     this.ready = false
@@ -48,12 +56,33 @@ class BrowserTag implements Tag {
     return this.size >= COMPONENTS_PER_TAG
   }
 
+  speedyInsert(el: HTMLStyleElement, css: string) {
+    const sheet = el.sheet
+    if (sheet === null || sheet === undefined) {
+      return
+    }
+    const match = css.match(CSS_RULE_SPLIT_RE)
+    if (match !== null && match !== undefined) {
+      for (let i = 0; i < match.length; i += 1) {
+        const rule = match[i]
+        /* eslint-disable */
+        sheet.insertRule(rule, rule.indexOf('@import') !== -1 ? 0 : sheet.cssRules.length)
+        /* eslint-enable */
+      }
+    }
+  }
+
   addComponent(componentId: string) {
     if (!this.ready) this.replaceElement()
-    if (this.components[componentId]) throw new Error(`Trying to add Component '${componentId}' twice!`)
+    if (this.components[componentId] !== undefined) {
+      // eslint-disable-next-line no-console
+      console.error(new Error(`Trying to add Component '${componentId}' twice!`))
+    }
 
     const comp = { componentId, textNode: document.createTextNode('') }
-    this.el.appendChild(comp.textNode)
+    if (!USE_SPEEDY) {
+      this.el.appendChild(comp.textNode)
+    }
 
     this.size += 1
     this.components[componentId] = comp
@@ -63,18 +92,30 @@ class BrowserTag implements Tag {
     if (!this.ready) this.replaceElement()
     const comp = this.components[componentId]
 
-    if (!comp) throw new Error('Must add a new component before you can inject css into it')
-    if (comp.textNode.data === '') comp.textNode.appendData(`\n/* sc-component-id: ${componentId} */\n`)
+    if (comp === undefined) {
+      // eslint-disable-next-line no-console
+      console.error(new Error('Must add a new component before you can inject css into it'))
+      return
+    }
 
-    comp.textNode.appendData(css)
-    if (name) {
+    if (USE_SPEEDY) {
+      this.speedyInsert(this.el, css)
+    } else {
+      if (comp.textNode.data === '') {
+        comp.textNode.appendData(`\n/* sc-component-id: ${componentId} */\n`)
+      }
+      comp.textNode.appendData(css)
+    }
+
+    if (name !== undefined) {
+      /* eslint-disable */
       const existingNames = this.el.getAttribute(SC_ATTR)
       this.el.setAttribute(SC_ATTR, existingNames ? `${existingNames} ${name}` : name)
+      /* eslint-enable */
     }
 
     const nonce = getNonce()
-
-    if (nonce) {
+    if (nonce !== null) {
       this.el.setAttribute('nonce', nonce)
     }
   }
@@ -131,7 +172,9 @@ export default {
     for (let i = 0; i < nodesLength; i += 1) {
       const el = nodes[i]
 
+      /* eslint-disable */
       tags.push(new BrowserTag(el, el.getAttribute(LOCAL_ATTR) === 'true', el.innerHTML))
+      /* eslint-enable */
 
       const attr = el.getAttribute(SC_ATTR)
       if (attr) {
